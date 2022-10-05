940ba25fe5b8b7f964c5c5eecc2d2abd544aa458
osmaryupanqui <osmar30asis@gmail.com>
2022-10-05 14:45:09 -0500

    v2

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
-console.log('placeholder')
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+function getComputerChoice() {
+    let result = '';
+    let randomNumber = Math.random();
+    if (randomNumber < 0.333) {
+        result = 'rock';
+    } else if (randomNumber > 0.333 && randomNumber < 0.666) {
+        result = 'paper';
+    } else {
+        result = 'scissors'
+    };
+    return result;
+}
+
+
