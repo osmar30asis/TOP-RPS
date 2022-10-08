684c547f076b9c555e7674b52026da6ccbbc46b8
osmaryupanqui <osmar30asis@gmail.com>
2022-10-08 14:16:01 -0500

    changed paper img and js

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundResult = ''
 
 const player = document.querySelector("#player-score")
 player.textContent = `Player score: ${playerScore}`
@@ -7,13 +8,31 @@ player.textContent = `Player score: ${playerScore}`
 const computer = document.querySelector("#computer-score")
 computer.textContent = `Computer score: ${computerScore}`
 
+const round = document.querySelector("#text-score")
+round.textContent = `Round result: ${roundResult}`
+
 let choices = document.querySelectorAll("button")
 
 let playerChoice = '';
+
 choices.forEach(function (button) {
     button.addEventListener("click", function () {
         playerChoice = button.className;
         playRound(getPlayerChoice(), getComputerChoice())
+        if (playerScore == 10) {
+            round.textContent = `You beated the machine :D`
+            playerScore = 0;
+            computerScore = 0;
+            player.textContent = `Player score: ${playerScore}`
+            computer.textContent = `Computer score: ${computerScore}`
+
+        } else if (computerScore == 10) {
+            round.textContent = `You got beated by the machine :S`
+            playerScore = 0;
+            computerScore = 0;
+            player.textContent = `Player score: ${playerScore}`
+            computer.textContent = `Computer score: ${computerScore}`
+        }
     })
 })
 
@@ -38,24 +57,24 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
     (playerSelection == 'paper' && computerSelection == 'rock') ||
     (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        console.log('You won!')
+        roundResult = 'You won!'
+        round.textContent = `Round result: ${roundResult}`
+
         playerScore += 1
         player.textContent = `Player score: ${playerScore}`
+
 
     } else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
     (playerSelection == 'paper' && computerSelection == 'scissors') ||
     (playerSelection == 'scissors' && computerSelection == 'rock')) {
-        console.log('You lost!')
+        roundResult = 'You lost!'
+        round.textContent = `Round result: ${roundResult}`
+
         computerScore += 1
         computer.textContent = `Computer score: ${computerScore}`
 
     } else {
-        console.log('Draw!')
-    }
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(getPlayerChoice(), getComputerChoice())
+        roundResult = 'Draw!'
+        round.textContent = `Round result: ${roundResult}`
     }
 }
